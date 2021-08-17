f23725371f4b9cd6b6f1dc777609a1bb84dd383b
Markus Zhang <45726499+photomz@users.noreply.github.com>
2021-08-17 10:48:10 +0800

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Fig`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Markus Zhang`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
